bcb321c0aa79e38fb252fd830fc79349bfa3031f
siddiqov <siddiqov@gmail.com>
2025-04-01 16:16:45 +0100

    The update code that contains Addition function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
+        System.out.println("Sum: " + add(5, 3));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
+
